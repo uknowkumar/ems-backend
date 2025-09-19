a322a8a139859e368e574349ff4d21d1a9a3cafb
kumar <uknow.kumar@gmail.com>
2025-09-19 20:45:24 +0530

    removed var

--- src/main/java/net/javaguides/ems/WebprojApplication.java
+++ src/main/java/net/javaguides/ems/WebprojApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebprojApplication {
 
 	public static void main(String[] args) {
-		int i=10;
+		
 		SpringApplication.run(WebprojApplication.class, args);
 		
 	}
