6879d8df3c984cd142e706176dcc6829efbbd7da
kumar <uknow.kumar@gmail.com>
2025-09-19 10:58:20 +0530

    first commit

---
+++ src/main/java/net/javaguides/ems/WebprojApplication.java
@@ -0,0 +1,13 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojApplication.class, args);
+		
+	}
+
+}
